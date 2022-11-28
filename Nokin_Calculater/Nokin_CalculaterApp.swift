7bd2bb10b4158fa27b76e7c5024bcf5c6f62cf3a
NiraiHayakawa <91765262+NiraiHayakawa@users.noreply.github.com>
2022-11-28 04:55:29 -0800

    Initial Commit

---
+++ Nokin_Calculater/Nokin_CalculaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nokin_CalculaterApp.swift
+//  Nokin_Calculater
+//
+//  Created by geo on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct Nokin_CalculaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
